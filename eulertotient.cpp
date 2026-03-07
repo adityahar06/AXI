1fa6cfe01972167e0fd0c31269431d7e06f985ec
adityahar06 <adityaharshu06@gmail.com>
2026-03-07 16:06:04 +0530

    euler totient code

---
+++ eulertotient.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int gcd(int a,int b){
+    if(b==0)return a;
+    return gcd(b,a%b);
+}
+int main(){
+    int n;
+    cin>>n;
+    int cnt=0;
+    for(int i=1;i<=n;i++){
+        if(gcd(n,i)==1)cnt++;
+    }
+    cout<<cnt<<endl;
+    return 0;
+}
